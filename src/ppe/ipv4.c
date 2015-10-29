b8e3b221247e0901ca9b7cfd5379833249f638fd
Simon Schmidt <simon.schmidt@htp-tel.de>
2015-10-29 07:46:48 +0100

    IPv4 checksum processing, packet creation

--- src/ppe/ipv4.c
+++ src/ppe/ipv4.c
@@ -13,6 +13,32 @@
 #include <ppe/endianess.h>
 #include <ppe/_lib_crc.h>
 
+/*
+ *             Figure 1: IPv4 packet
+ *
+ *   0                   1                   2                   3
+ *    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |Version|  IHL  |   DSCP    |ECN|          Total Length         |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |         Identification        |Flags|      Fragment Offset    |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |  Time to Live |    Protocol   |         Header Checksum       |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |                       Source Address                          |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |                    Destination Address                        |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *
+ *      Additionally/Optionally:
+ *
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *   |                          Options                              |
+ *   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *     .
+ *     .
+ *     .
+ */
 
 /*
  *    EthernetFrameHeader
@@ -44,20 +70,55 @@ typedef net_struct_begin{
 
 typedef void* Pointer;
 
-static uint16_t ipchecksum(uint8_t* pkg, uintptr_t len){
+
+/*
+ * @brief Calculates the IPv4 header checksum.
+ * @param[in] hdr   Pointer to the header.
+ * @param[in] ihl2  number of 16-bit words in the header. MUST BE (ihl * 2)!
+ *
+ * This function calculates the IPv4 checksum for an IPv4 header. It assumes
+ * that the ihl2-value is always at least 10 (5 * 2).
+ *
+ * The algorithm always works correctly without the need for byte-order swapping.
+ */
+static uint16_t ipHeaderSum(uint16_t* hdr, int ihl2){
+	int i;
 	register uint32_t check = 0;
-	while(len>1){
-		check+=((uint16_t)pkg[1])|(((uint16_t)pkg[0])<<8);
-		len-=2;
-		pkg+=2;
-	}
-	if(len>0) check += pkg[0];
+
+	/*
+	 * While the checksum is calculated, the checksum field in the header is
+	 * temporarily filled up with zero.
+	 * 
+	 * We implement this by skipping this field in the checksum algorithm.
+	 * By doing so, we do not need to set the field to zero in the packet buffer
+	 * or to copy the packet header into another buffer for processing.
+	 */
+
+	/*
+	 * Process the first 5 16-bit words.
+	 */
+	for(i=0;i<5;++i,++hdr) check+=*hdr;
+
+	/*
+	 * Skip the checksum field.
+	 */
+	++hdr;
+
+	/*
+	 * Process the rest of the header.
+	 */
+	for(i=6;i<ihl2;++i,++hdr) check+=*hdr;
+
+	/*
+	 * Folds the result.
+	 */
 	while(check>>16) check = (check&0xffff)+(check>>16);
 	return check;
 }
 
+
 int ppe_createPacket_ipv4(ppeBuffer *packet, IPV4_FrameInfo *info) {
-	uintptr_t length;
+	uintptr_t length;int i;
 	Pointer beginHeader, endHeader,endPacket;
 	IPv4PacketHeader *header;
 
@@ -96,11 +157,12 @@ int ppe_createPacket_ipv4(ppeBuffer *packet, IPV4_FrameInfo *info) {
 		(info->fragmentOffset&0x1fff)|(info->fragmentFlags<<13)
 	);
 	header->ttl                  =  info->ttl;
-	header->protocol             =  info->protocol;
-	header->checksum             =  encBE16(
-				ipchecksum(beginHeader,endHeader-beginHeader) );
+	header->protocol             =  info->protocol;	
 	header->srcIPv4Addr          =  info->local;
 	header->dstIPv4Addr          =  info->remote;
+	for(i=5; i<info->ihl ;++i)
+		((Word*)beginHeader)[i].value = encBE32( info->options[i-5] );
+	header->checksum             =  ipHeaderSum( beginHeader, info->ihl * 2 );
 
 	/*
 	 * Assign the new boundaries to the packet.
@@ -144,10 +206,9 @@ int ppe_parsePacket_ipv4(ppeBuffer *packet, IPV4_FrameInfo *info) {
 	info->fragmentOffset  =  fragment & 0x1fff;
 	info->ttl             =  header->ttl;
 	info->protocol        =  header->protocol;
-	info->checksum        =  decBE16( header->checksum );
+	info->checksum        =  header->checksum;
 	info->remote          =  header->srcIPv4Addr;
 	info->local           =  header->dstIPv4Addr;
-
 
 	/*
 	 * Calculate the end of the packet.
@@ -163,9 +224,12 @@ int ppe_parsePacket_ipv4(ppeBuffer *packet, IPV4_FrameInfo *info) {
 	if( (beginHeader+(4*ihl)) > endPacket ) return ERROR_BUFFER_OVERFLOW;
 	if( ihl<5 || ihl>15 ) return ERROR_BUFFER_OVERFLOW;
 	for(i=5; i<ihl ;++i)
-		info->options[i-5] = ((Word*)beginHeader)[i].value;
+		info->options[i-5] = decBE32( ((Word*)beginHeader)[i].value );
 
-	if( info->checksum != ipchecksum(beginHeader,endHeader-beginHeader) )
+	/*
+	 * Perform checksum checking.
+	 */
+	if( header->checksum != ipHeaderSum( beginHeader, ihl * 2 ) )
 		return ERROR_CHECKSUM_MISMATCH;
 
 	/*
